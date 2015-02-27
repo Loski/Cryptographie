b8efd0698f87f8788f0f847b5b0af99abe5c8f5f
Yamihoshi <Yamihoshi@users.noreply.github.com>
2015-02-27 19:23:18 +0100

    Ajout Decryptage avec clé
    - BUG on peut pas les enchaîner ??!!

--- js/vigenere.js
+++ js/vigenere.js
@@ -35,6 +35,44 @@
 	$("#textecode").text(crypt);
 }
 
+function vigenere_decrypt(texte,cle){
+	console.log("DECRYPT");
+	var decrypt="";
+	for(var i=0,j=0;i<texte.length;i++)
+	{
+		if(j==cle.length) //on reboucle sur le début de la clé
+			j=0;
+		
+		var cara=texte.charAt(i);
+		var indice=cara.charCodeAt(0);
+		
+		if(indice>="a".charCodeAt(0) && indice<="z".charCodeAt(0)) //Minuscule
+		{
+			cara=cara.toUpperCase();
+			indice=cara.charCodeAt(0);
+			var somme=(indice-cle.charAt(j).charCodeAt(0))%26;
+			var val = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(somme);
+			decrypt+=val.toLowerCase();
+			j++;
+		}
+		
+		else if((indice>="A".charCodeAt(0) && indice<="Z".charCodeAt(0))) //Maj
+		{
+			var somme=(indice-cle.charAt(j).charCodeAt(0))%26; //position dans l'alphabet
+			var val = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(somme);
+			decrypt+=val;
+			j++;
+		}
+		else
+		{
+			decrypt+=cara;
+		}
+		
+	}
+	
+	$("#texteclair").text(decrypt);
+}
+
 $(document).ready(function()
 { 
 	$('#cryptVig').mousedown(function()
@@ -44,6 +82,14 @@ $(document).ready(function()
 			var cle = document.getElementById('keyVig').value;
 			var cryptageCara = document.getElementById('cryptOtherCaract').checked;
 			vigenere_crypt(texte,cle,cryptageCara);
+		});
+		
+	$('#decryptVig').mousedown(function()
+		{
+			console.log("VIGENERE INIT");
+			var texte=document.getElementById('textecode').value;
+			var cle = document.getElementById('keyVig').value;
+			vigenere_decrypt(texte,cle);
 		});
 
 
