67a579b7ce4c2c08a98abe5f8c4ea11f19c1ae57
Yamihoshi <Yamihoshi@users.noreply.github.com>
2015-03-26 19:42:28 +0100

    shit

--- web/js/cryptanalyse.js
+++ web/js/cryptanalyse.js
@@ -277,13 +277,14 @@ function decryptCesarAppel(key){
 function key_Cesar(text,alphabet,iteration,changeLetter){
 
 		var array=arrayFreqApparition(1);
-		var caraMaxOcurrence = alphabet.indexOf(maxFreq(text,1,changeLetter));
+		var caraMaxOcurrence = alphabet.indexOf(maxFreq(text,1,changeLetter).label);
 		var letterMostUse = alphabet.indexOf(array[iteration].toUpperCase());
 		//console.log(maxCharacterFrequence(text,1,changeLetter),array[iteration].toUpperCase());
 		var key =(caraMaxOcurrence-letterMostUse)%alphabet.length;
 		if(key<0)
 			key+=alphabet.length;
 	
+		console.log(maxFreq(text,1,changeLetter).label,array[iteration].toUpperCase());
 		return key;
 }
 
